64ad6fe8504db0c0adca3b01f8923bc2a2067b60
OseiasFreedom <oseiasfreedom01@gmail.com>
2026-05-18 08:43:53 -0300

    fix: liberar CORS para producao

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const routes     = require('./routes');
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.FRONTEND_URL || '*', credentials: true }));
+app.use(cors({ origin: '*', credentials: false }));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
